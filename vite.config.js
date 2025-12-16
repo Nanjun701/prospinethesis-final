49fe169283b7870010c0ab64cdc9326c1bf0e8dc
Nanjun701 <liun266@newschool.edu>
2025-12-16 10:22:52 -0500

    Build: generate production files for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-aaf627ad9f61aaa6a4fea1130d1e73f912d4418
+
 export default defineConfig({
+  base: '/prospinethesis-final/', 
   plugins: [react()],
-})
+});
